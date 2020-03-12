4ff3854b456461ec1fa611f99c61125fdba13a5a
VargaCatalin <catavarga@yahoo.com>
2020-03-12 16:07:26 +0200

    Studied methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -15,6 +15,7 @@ public class App
         carReference.colour ="red";
         carReference.maxSpeed = 260;
         carReference.mileage = 6.2;
+        carReference.fuelLevel = 100;
 
 
 //        carReference.engine - new Engine();
@@ -26,6 +27,10 @@ public class App
         engine1.capacity = 2000;
 
         carReference.engine = engine1;
+        double currentDistance = carReference.accelerate(60, 1);
+
+        System.out.println("Current distance: " + currentDistance);
+
         // One object with two references
 
         System.out.println("Engine details");
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -10,6 +10,33 @@ public class Vehicle {
     String colour;
     double fuelLevel;
     boolean damaged;
+    double totalDistance;
 
+    public double accelerate(double speed, double durationInHours) {
+
+        System.out.println(name + "is accelerating with " + speed + "km/ for " + durationInHours + "h");
+
+        //local variable
+        double traveledDistance = speed * durationInHours;
+
+        System.out.println("Traveled distance: " + traveledDistance);
+
+        totalDistance += traveledDistance;
+
+        System.out.println("Total traveled distance: " + totalDistance);
+
+        double usedFuel = traveledDistance * mileage / 100;
+
+        fuelLevel = fuelLevel - usedFuel;
+        // same result as the instruction above
+        //fuelLevel -= usedFuel;
+
+        System.out.println("Remaining fuel: " + fuelLevel);
+
+
+        return traveledDistance;
+
+
+    }
 
 }
